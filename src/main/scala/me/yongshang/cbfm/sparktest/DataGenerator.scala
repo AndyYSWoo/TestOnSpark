0f425fee2d2c65c1aeff8f335963e623e7d7be6b
Andy <andyyswoo@gmail.com>
2016-11-01 18:16:44 +0800

    mod DataGenerator, add dep, find bug ArrayIndexOutOfBounds of int column

--- src/main/scala/me/yongshang/cbfm/sparktest/DataGenerator.scala
+++ src/main/scala/me/yongshang/cbfm/sparktest/DataGenerator.scala
@@ -1,8 +1,73 @@
 package me.yongshang.cbfm.sparktest
 
+import java.util.Date
+
+import me.yongshang.cbfm.CBFM
+import org.apache.spark.sql.SparkSession
+
 /**
   * Created by yongshangwu on 2016/11/1.
   */
 object DataGenerator {
+  val spark = SparkSession
+    .builder
+    .appName("TPC-H data generator")
+    .getOrCreate
+  def main(args: Array[String]): Unit = {
+    CBFM.DEBUG = false
+    CBFM.ON = true
+    CBFM.desired_false_positive_probability_ = 0.1
+    CBFM.setIndexedDimensions(Array("c_custkey", "c_nationkey", "c_mksegment"))
+    CBFM.reducedimensions = Array(6)
 
+    val customer = spark.sparkContext
+      .textFile("/Users/yongshangwu/Downloads/tpch_2_17_0/dbgen/customer.tbl")
+      .map(_.split("\\|"))
+      .take(3)
+      .map(attrs => Customer(attrs(0).toInt, attrs(1), attrs(2)
+                              , attrs(3).toInt, attrs(4), attrs(5).toDouble
+                              , attrs(6), attrs(7)))
+    val customerDF = spark.createDataFrame(customer)
+    customerDF.createOrReplaceTempView("customer")
+    customerDF.write.parquet("customer.parquet")
+
+    val readCustomer = spark.read.parquet("customer.parquet")
+    readCustomer.createOrReplaceTempView("customer")
+
+    val results = spark.sql("SELECT c_custkey FROM customer WHERE c_nationkey=15 AND c_mksegment='BUILDING'")
+    results.show
+  }
 }
+
+
+
+case class Part(p_partkey: Int, p_name: String, p_mfgr: String
+               , p_brand: String, p_type: String, p_size: Int
+               , p_container: String, p_retailprice: Double, p_comment: String)
+
+case class Supplier(s_suppkey: Int, s_name: String, s_address: String
+                   , s_nationkey: String, s_phone: String, s_acctbal: Double
+                   , s_comment: String)
+
+case class Partsupp(ps_partkey: Int, ps_suppkey: Int, ps_availqty: Int
+                    , ps_supplycost: Double, ps_comment: String)
+
+case class Customer(c_custkey: Int, c_name: String, c_address: String
+                     , c_nationkey: Int, c_phone: String, c_acctbal: Double
+                     , c_mksegment: String, c_comment: String)
+
+case class Orders(o_orderkey: Int, o_custkey: Int, o_orderstatus: Int
+                  , o_totalprice: Double, o_orderdate: Date, o_orderpriority: String
+                  , o_clerk: String, o_shippriority: String, o_comment: String)
+
+case class Lineitem(l_orderkey: Int, l_partkey: Int, l_suppkey: Int
+                   , l_linenumber: Int, l_quantity: Double, l_extendedprice: Double
+                   , l_discount: Double, l_tax: Double, l_returnflag: Double
+                   , l_linestatus: Double, l_shipdate: Date, l_commitdate: Date
+                   , l_receiptdate: Date, l_shipinstruct: String, l_shipmode: String
+                   , l_comment: String)
+
+case class Nation(n_nationkey: Int, n_name: String, n_regionkey: String
+                 , n_comment: String)
+
+case class Region(r_regionkey: Int, r_name: String, r_comment: String)
--- src/main/scala/me/yongshang/cbfm/sparktest/SparkTest.scala
+++ src/main/scala/me/yongshang/cbfm/sparktest/SparkTest.scala
@@ -16,6 +16,7 @@ object SparkTest {
   def main(args: Array[String]) {
 
     // config CBFM params
+    CBFM.DEBUG = true
     CBFM.ON = true
     CBFM.desired_false_positive_probability_ = 0.1
     CBFM.setIndexedDimensions(Array("name", "age", "balance"))
@@ -24,7 +25,16 @@ object SparkTest {
     query()
   }
   def createFile(): Unit ={
-    val persons = Seq(Person("Jack", 21, 1000), Person("Jason", 35, 5000), Person("James", 40, 2000), Person("Someone", 35, 3000))
+    val persons = Seq(
+      Person("Jack", 21, 1000)
+      , Person("Jason", 35, 5000)
+      , Person("James", 40, 2000)
+      , Person("Someone", 35, 3000)
+      , Person("Customer#000000003", 35, 3000)
+//      , Person("MG9kdTD2WBHm", 35, 3000)
+//      , Person("11-719-748-3364", 35, 3000)
+//      , Person(" deposits eat slyly ironic, even instructions. express foxes detect slyly. blithely even accounts abov", 35, 3000)
+    )
     val personDF = sparkContext.createDataFrame(persons)
     personDF.show()
     personDF.createOrReplaceTempView("persons")
