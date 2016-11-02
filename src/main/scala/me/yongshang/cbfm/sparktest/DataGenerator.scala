02d6f6cc349017fd7987969b2813cd732ae901d4
Andy <andyyswoo@gmail.com>
2016-11-02 18:15:07 +0800

    add blocks record

--- src/main/scala/me/yongshang/cbfm/sparktest/DataGenerator.scala
+++ src/main/scala/me/yongshang/cbfm/sparktest/DataGenerator.scala
@@ -1,23 +1,27 @@
 package me.yongshang.cbfm.sparktest
 
+import java.io.{File, FileWriter, PrintWriter}
 import java.util.Date
 
 import me.yongshang.cbfm.CBFM
+import org.apache.spark.deploy.SparkHadoopUtil
 import org.apache.spark.sql.SparkSession
 
 /**
   * Created by yongshangwu on 2016/11/1.
   */
 object DataGenerator {
+  val blockSize = 1 * 1024 * 1024
   val spark = SparkSession
     .builder
     .appName("TPC-H data generator")
     .config("parquet.task.side.metadata", true)
     .config("parquet.enable.dictionary",false)
+//    .config("parquet.block.size", blockSize)
     .getOrCreate
   def main(args: Array[String]): Unit = {
     CBFM.DEBUG = false
-    CBFM.ON = true
+    CBFM.ON = false
     CBFM.desired_false_positive_probability_ = 0.1
     CBFM.setIndexedDimensions(Array("c_custkey", "c_nationkey", "c_mksegment"))
     CBFM.reducedimensions = Array(6)
@@ -26,17 +30,31 @@ object DataGenerator {
       .textFile("/Users/yongshangwu/Downloads/tpch_2_17_0/dbgen/customer.tbl")
       .map(_.split("\\|"))
       .map(attrs => Customer(attrs(0).toInt, attrs(1), attrs(2)
-      , attrs(3).toInt, attrs(4), attrs(5).toDouble
-      , attrs(6), attrs(7)))
+        , attrs(3).toInt, attrs(4), attrs(5).toDouble
+        , attrs(6), attrs(7))).collect()  // why .collect is needed?
     val customerDF = spark.createDataFrame(customer)
     customerDF.createOrReplaceTempView("customer")
+    val start = System.currentTimeMillis()
     customerDF.write.parquet("customer.parquet")
-
+    println("==========Time: "+(System.currentTimeMillis()-start))
     val readCustomer = spark.read.parquet("customer.parquet")
     readCustomer.createOrReplaceTempView("customer")
-
     val results = spark.sql("SELECT c_custkey FROM customer WHERE c_nationkey=15 AND c_mksegment='BUILDING'")
-    println(results.count())
+    println("==========result count: "+results.count())
+
+    var totalBlocks: Long = 0;
+    var skippedBlocks: Long = 0;
+    val iter = SparkHadoopUtil.get.conf.iterator()
+    while(iter.hasNext) {
+      val entry = iter.next
+      if (entry.getKey.startsWith(("parquet.cbfm.totalblocks."))) {
+        totalBlocks = totalBlocks + SparkHadoopUtil.get.conf.getInt(entry.getKey, -1)
+      }
+      if (entry.getKey.startsWith("parquet.cbfm.skipblocks.")) {
+        skippedBlocks = skippedBlocks + SparkHadoopUtil.get.conf.getInt(entry.getKey, -1)
+      }
+    }
+    println("==========Total blocks: "+totalBlocks+", skipped blocks: "+skippedBlocks)
   }
 }
 
