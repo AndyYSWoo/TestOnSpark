e6412dfc76a1ddac5066738debc31d92dae52f2f
Andy <andyyswoo@gmail.com>
2016-12-08 11:01:34 +0800

    change file writing to hdfs, add dns data generator & query

---
+++ src/main/scala/me/yongshang/cbfm/sparktest/DnsQuery.scala
@@ -0,0 +1,8 @@
+package me.yongshang.cbfm.sparktest
+
+/**
+  * Created by yongshangwu on 2016/12/8.
+  */
+object DnsQuery {
+
+}
--- src/main/scala/me/yongshang/cbfm/sparktest/TpchQuery.scala
+++ src/main/scala/me/yongshang/cbfm/sparktest/TpchQuery.scala
@@ -4,6 +4,9 @@ import java.io._
 import java.nio.file.{Paths, Files}
 import java.sql.Date
 
+import org.apache.commons.io.IOUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{Path, FileSystem}
 import org.apache.parquet.filter2.compat.RowGroupFilter
 import org.apache.spark.sql.DataFrame
 
@@ -13,6 +16,7 @@ import org.apache.spark.sql.DataFrame
 object TpchQuery {
   val spark = DataGenerator.spark
   var dataSourceFolder: String = null
+  var fs: FileSystem = null
   def main(args: Array[String]) {
     // File path config
     dataSourceFolder = args(0)
@@ -32,44 +36,25 @@ object TpchQuery {
     DataGenerator.setUpMDBF(index.equals("mdbf"), dimensions)
     DataGenerator.setUpCMDBF(index.equals("cmdbf"), dimensions)
 
-    /* DEBUG USAGE: SINGLE TABLE
-    DataGenerator.generateFile(dataSourceFolder)
-    DataGenerator.readAndCreateTable()
-    */
-
     // Write out with index
-//    denormalized.write.parquet("denormalized.parquet")
+    val parquetDir = args(3)
+    val parquetFile = parquetDir+"denormalized.parquet"
+    denormalized.write.parquet(parquetFile)
 
     // Read denormalized table & query
-    spark.read.parquet("denormalized.parquet").createOrReplaceTempView("denormalized")
-
-    /*
-    var testQuery = "SELECT SUM(l_extendedprice) / 7.0 AS avg_yearly FROM denormalized WHERE p_brand = 'Brand#55' AND p_container = 'SM BAG' AND l_quantity < ( SELECT 0.2 * AVG(l_quantity) FROM lineitem WHERE l_partkey = p_partkey )"
-    testQuery = "select DATE_FORMAT(o_orderdate, '%Y') as o_year, l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation from denormalized, nation n1, nation n2, region where c_nationkey = n1.n_nationkey and n1.n_regionkey = r_regionkey and r_name = 'ASIA' and s_nationkey = n2.n_nationkey and o_orderdate between date '1995-01-01' and date '1996-12-31'\n\t\t\tand p_type = 'PROMO BURNISHED TIN'"
-    val query = "17"
-    RowGroupFilter.query = query
-
-    // Writer
-    val resultFile: File = new File(RowGroupFilter.filePath + "query"+query+"-time")
-    if (!resultFile.exists) resultFile.createNewFile
-    val pw: PrintWriter = new PrintWriter(new FileWriter(resultFile, true))
-    var totalTime = 0.0
-    val start = System.currentTimeMillis()
-    val result = spark.sql(testQuery)
-    val rowCount = result.count()
-    val time = System.currentTimeMillis() - start
-    totalTime += time
-    pw.write("query index denormalized: "+time + " ms. "+rowCount+"rows returned\n")
-    pw.write("=======\n")
-    pw.write("\tavg: " + (totalTime / 1))
-    pw.flush
-    pw.close
-    */
+    spark.read.parquet(parquetFile).createOrReplaceTempView("denormalized")
 
     // Query
+    setupFS()
     val queryCount = 1
     queryAndRecord("8", queryCount)
     queryAndRecord("17", queryCount)
+  }
+
+  def setupFS(): Unit ={
+    val conf = new Configuration()
+    conf.set("fs.defaultFS", "hdfs://tina:9000")
+    fs = FileSystem.get(conf)
   }
 
   def loadDataAndDenormalize(): DataFrame ={
@@ -155,17 +140,17 @@ object TpchQuery {
   }
 
   def loadQueries(queryPath: String): Array[String] ={
-    val fileContent = new String(Files.readAllBytes(Paths.get(queryPath)));
+    val file = fs.open(new Path(queryPath))
+    val fileContent = new String(IOUtils.toByteArray(file));
     val queries = fileContent.split("====")
-    return queries;
+    queries
   }
 
   def queryAndRecord(query: String, count: Int): Unit ={
-    RowGroupFilter.query = query;
     // Writer
-    val resultFile: File = new File(RowGroupFilter.filePath + "query"+query+"-time")
-    if (!resultFile.exists) resultFile.createNewFile
-    val pw: PrintWriter = new PrintWriter(new FileWriter(resultFile, true))
+    val timePath = new Path(RowGroupFilter.filePath + "query"+query+"-time")
+    val output = fs.create(timePath)
+    val pw = new PrintWriter(output)
 
     // Query
     val queries = loadQueries(dataSourceFolder + "queries"+query+".sql")
@@ -180,7 +165,7 @@ object TpchQuery {
       pw.write("query index "+i+": "+time + " ms. "+rowCount+"rows returned\n")
     }
     pw.write("=======\n")
-    pw.write("\tavg: " + (totalTime / count))
+    pw.write("\tavg: " + (totalTime / count)+"\n")
 
     pw.flush
     pw.close
@@ -191,10 +176,11 @@ object TpchQuery {
 
   def collectResults(query: String, queryCount: Int): Unit ={
     // index load time
-    var path = RowGroupFilter.filePath + "query" + query + "-index-load-time"
-    var recordFile = new File(path)
-    if(recordFile.exists()){
-      val content = new String(Files.readAllBytes(Paths.get(path)))
+    var path = new Path(RowGroupFilter.filePath + "query" + query + "-index-load-time")
+    if(fs.exists(path)){
+      val in = fs.open(path)
+      val content = new String(IOUtils.toByteArray(in))
+      in.close
       val lines = content.split("\n")
       var line: String = null
       var totalTime: Double = 0.0
@@ -204,7 +190,7 @@ object TpchQuery {
         count += 1
       }
 
-      val pw = new PrintWriter(new FileWriter(recordFile, true))
+      val pw = new PrintWriter(fs.append(path))
       pw.write("=======\n")
       pw.write("total: "+totalTime+" ms\n")
       pw.write("per query: "+(totalTime/queryCount)+" ms\n")
@@ -214,10 +200,11 @@ object TpchQuery {
     }
 
     // skip
-    path = RowGroupFilter.filePath + "query" + query + "-skip"
-    recordFile = new File(path)
-    if(recordFile.exists()){
-      val lines = new String(Files.readAllBytes(Paths.get(path))).split("\n")
+    path = new Path(RowGroupFilter.filePath + "query" + query + "-skip")
+    if(fs.exists(path)){
+      val in = fs.open(path)
+      val lines = new String(IOUtils.toByteArray(in)).split("\n")
+      in.close
       var line: String = null
       var totalBlocks = 0
       var skippedBlocks = 0
@@ -230,7 +217,7 @@ object TpchQuery {
         scannedRows += Integer.valueOf(tokens(8))
         skippedRows += Integer.valueOf(tokens(11))
       }
-      val pw = new PrintWriter(new FileWriter(recordFile, true))
+      val pw = new PrintWriter(fs.append(path))
       pw.write("=======\n")
       pw.write("blocks: total "+totalBlocks+", skipped "+skippedBlocks+", scanned "+(totalBlocks-skippedBlocks)+"\n")
       pw.write("rows: total "+(scannedRows+skippedRows)+", skipped "+skippedRows+", scanned "+scannedRows+"\n")
@@ -239,10 +226,12 @@ object TpchQuery {
     }
 
     // index create time
-    path = RowGroupFilter.filePath + "index-create-time"
-    recordFile = new File(path)
-    if(recordFile.exists()){
-      val lines = new String(Files.readAllBytes(Paths.get(path))).split("\n")
+    path = new Path(RowGroupFilter.filePath + "index-create-time")
+    if(fs.exists(path)){
+      val in = fs.open(path)
+      val lines = new String(IOUtils.toByteArray(in)).split("\n")
+      in.close
+      if(lines.contains("=======")) return // collect index create time & space only once
       var line: String = null
       var totalTime: Double = 0.0
       var count = 0
@@ -250,7 +239,7 @@ object TpchQuery {
         totalTime += Integer.valueOf(line.split(" ")(0))
         count += 1
       }
-      val pw = new PrintWriter(new FileWriter(recordFile, true))
+      val pw = new PrintWriter(fs.append(path))
       pw.write("=======\n")
       pw.write("total: "+totalTime+" ms\n")
       pw.write("per query: "+(totalTime/queryCount)+" ms\n")
@@ -260,10 +249,11 @@ object TpchQuery {
     }
 
     // index space
-    path = RowGroupFilter.filePath + "index-space"
-    recordFile = new File(path)
-    if(recordFile.exists()){
-      val lines = new String(Files.readAllBytes(Paths.get(path))).split("\n")
+    path = new Path(RowGroupFilter.filePath + "index-space")
+    if(fs.exists(path)){
+      val in = fs.open(path)
+      val lines = new String(IOUtils.toByteArray(in)).split("\n")
+      in.close
       var line: String = null
       var totalSpace: Double = 0.0
       var count = 0
@@ -271,7 +261,7 @@ object TpchQuery {
         totalSpace += java.lang.Double.valueOf(line.split(" ")(0))
         count += 1
       }
-      val pw = new PrintWriter(new FileWriter(recordFile, true))
+      val pw = new PrintWriter(fs.append(path))
       pw.write("=======\n")
       pw.write("total: "+totalSpace+" MB\n")
       pw.write("per block: "+(totalSpace/count)+" MB\n")
